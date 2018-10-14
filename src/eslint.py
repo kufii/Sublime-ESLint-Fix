6c166b400262ac5c5bcdf25b4904cf61cab32947
Adrien Pyke <adpyke@gmail.com>
2018-10-13 23:43:49 -0400

    refactoring

--- src/eslint.py
+++ src/eslint.py
@@ -7,8 +7,8 @@ import sublime
 from . import preferences, pathutil
 
 
-def fix(text, filename):
-    return _run(text, filename)
+_IS_WINDOWS = sublime.platform() == 'windows'
+_ERROR_EXIT_CODE = 2
 
 
 def _get_node_path():
@@ -25,24 +25,17 @@ def _get_eslint_path():
     return eslint
 
 
-def _run(text, filepath):
+def _get_command(directory, filename):
     cmd = None
-
-    filename = None
-    directory = None
-    if filepath:
-        (directory, filename) = os.path.split(filepath)
 
     local_eslint = preferences.get_local_eslint_path(directory)
     if local_eslint:
         cmd = [_get_node_path(), local_eslint]
     else:
-        if sublime.platform() == 'windows':
+        if _IS_WINDOWS:
             cmd = [_get_eslint_path()]
         else:
             cmd = [_get_node_path(), _get_eslint_path()]
-
-    config = preferences.get_config_path(directory)
 
     cmd.extend([
         '--stdin',
@@ -51,29 +44,37 @@ def _run(text, filepath):
         # Default formatter doesn't output formatted code.
         '--format=json'
     ])
+
     if filename:
-        # Report file name to eslint.
         cmd.extend(['--stdin-filename', filename])
+
+    config = preferences.get_config_path(directory)
     if config:
         cmd.extend(['--config', config])
 
+    return cmd
+
+
+def fix(text, filepath):
+    directory = None
+    filename = None
+    if filepath:
+        (directory, filename) = os.path.split(filepath)
+
+    cmd = _get_command(directory, filename)
+
     try:
-        proc = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE,
-                     cwd=directory,
-                     shell=(sublime.platform() == 'windows'))
+        proc = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE, cwd=directory, shell=_IS_WINDOWS)
         (stdout, stderr) = proc.communicate(text.encode('utf-8'))
-        # Possible exit codes:
-        #  0 - no linting issues. Might or might not provide formatting.
-        #  1 - has linting issues. Might or might not provide formatting.
-        #  2 - serious error, e.g. missing configuration; error in stderr
-        if proc.returncode <= 1:
-            obj = json.loads(stdout.decode('utf-8'), encoding='utf-8')
-            # First list item is the result for formatted file.
-            if 'output' in obj[0]:
-                return obj[0].get('output')
-        else:
-            err = stderr.decode('utf-8')
-            print('ESlint Fix: File not formatted due to an error "%s".' % err)
+
+        if proc.returncode == _ERROR_EXIT_CODE:
+            print('ESlint Fix: File not formatted due to an error "%s".' % stderr.decode('utf-8'))
+            return None
+
+        obj = json.loads(stdout.decode('utf-8'), encoding='utf-8')
+        # First list item is the result for formatted file.
+        if 'output' in obj[0]:
+            return obj[0].get('output')
     except OSError:
         raise Exception('Error occured when running: %s' % ' '.join(cmd))
 
